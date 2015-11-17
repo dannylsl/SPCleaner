ef49d39dfe876856fa49388e8c8c0e0ec0288b91
johnzhong3599 <john3599@163.com>
2015-11-17 19:14:37 +0800

    LOG Function
    INF \ WAN \ ERR message print

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+#define INF 1
+#define WAN 2
+#define ERR 3
+
+void print(int level, char *msg) {
+	switch(level){
+		case INF:
+			printf("[INF] %s\n",msg);
+			break;
+		case WAN:
+			printf("[WAN] %s\n",msg);
+			break;
+		case ERR:
+			printf("[ERR] %s\n",msg);
+			break;
+		default:
+			break;
+	}
+	
+	return;
+}
+
 int main(int argc, char *argv[]) {
 
 	printf("Hello SPCleaner!\n");
+	print(INF, "Hello World!\n");
 	return 0;
 }
