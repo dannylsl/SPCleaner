fca90d0c821b78e46b75a11d38a9c115837b5dfe
johnzhong <john3599@163.com>
2015-11-17 19:29:02 +0800

    Code review modification
    1. char *msg -> const char *msg
    2. remove useless space & tab

--- main.c
+++ main.c
@@ -4,27 +4,29 @@
 #define WAN 2
 #define ERR 3
 
-void print(int level, char *msg) {
-	switch(level){
-		case INF:
-			printf("[INF] %s\n",msg);
-			break;
-		case WAN:
-			printf("[WAN] %s\n",msg);
-			break;
-		case ERR:
-			printf("[ERR] %s\n",msg);
-			break;
-		default:
-			break;
-	}
-	
-	return;
+void print(int level, const char *msg) {
+    switch(level){
+        case INF:
+            printf("[INF] %s\n",msg);
+            break;
+        case WAN:
+            printf("[WAN] %s\n",msg);
+            break;
+        case ERR:
+            printf("[ERR] %s\n",msg);
+            break;
+        default:
+            break;
+    }
+
+    return;
 }
 
 int main(int argc, char *argv[]) {
 
-	printf("Hello SPCleaner!\n");
-	print(INF, "Hello World!\n");
-	return 0;
+    printf("Hello SPCleaner!\n");
+    print(INF, "Hello World!\n");
+    print(WAN, "Hello World!\n");
+    print(ERR, "Hello World!\n");
+    return 0;
 }
