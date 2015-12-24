5b0e9f317591fd4c6fbd666def309af43b48bbbc
victorlyz <zhuan123tou@gmail.com>
2015-12-24 23:44:35 +0800

    Majorizate the function:SPCTrim
    1.can make directory automatically;
    2.can find the file in other directory and deal with it.

--- lib/SPCTrim.c
+++ lib/SPCTrim.c
@@ -1,6 +1,6 @@
 #include "SPCTrim.h"
 
-int SPC_TRIM(char *filename) {
+int SPC_TRIM(char *path) {
 
     /*定义私有变量*/
     FILE      *pRead = NULL;
@@ -13,25 +13,44 @@ int SPC_TRIM(char *filename) {
     float     effic = 0.00;
     char      outputfile[MAX_LINE] = "./copy/";
     char      fline[MAX_LINE];
+    char      *filename = NULL;
 
     /*变量初始化*/
     memset(fline, 0x00, MAX_LINE);
 
+    unResult = SPC_INIT();
+    if(unResult != 0) {
+        SPC_MSG(LOGERR, "SPC_INIT ERROR!");
+        return 1;
+    }
+
+    /*创建输出目录*/
+    unResult = mkdir("./copy", 0755);
+    if(unResult != 0) {
+        SPC_MSG(LOGERR, "Make directory fail!");
+        return 1;
+    }
+
+    /*获取输出文件路径*/
+    filename = strrchr(path, 47) + 1;
     strcat(outputfile, filename);
 
-    pRead = fopen(filename, "r");
+    /*打开处理文件-读取*/
+    pRead = fopen(path, "r");
     if(NULL == pRead) {
         SPC_MSG(LOGERR, "File open fail!");
         return 1;
     }
-    size1 = getFileSize(filename);
+    size1 = getFileSize(path);
 
+    /*打开输出文件-新建写入*/
     pCopy = fopen(outputfile, "w");
     if(NULL == pCopy) {
         SPC_MSG(LOGERR, "File open fail!");
         return 1;
     }
 
+    /*去空格处理*/
     while(fgets(fline, MAX_LINE, pRead) != NULL) {
         lenth = strlen(fline);
         if(1 == lenth) {
@@ -64,6 +83,7 @@ int SPC_TRIM(char *filename) {
             }
         }
     }
+
     unResult = fclose(pRead);
     if(unResult != 0) {
         SPC_MSG(LOGERR, "File [pRead] close fail !");
@@ -73,6 +93,7 @@ int SPC_TRIM(char *filename) {
         SPC_MSG(LOGERR, "File [pCopy] close fail!");
     }
 
+    /*输出文件前后差异*/
     size2 = getFileSize(outputfile);
     effic = (float)size2 / size1 * 100;
     printf("size1 = %ld ,size2 = %ld \n", size1, size2);
