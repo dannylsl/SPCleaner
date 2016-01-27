0d55f7b4b3e346d8f58c0cf108fe6bccd0c4a527
dannylsl <dannylsl@hotmail.com>
2016-01-27 15:02:32 +0800

    Warning fix in trim.c

--- test/trim.c
+++ test/trim.c
@@ -20,37 +20,37 @@ int main() {
     SPC_MSG(LOGINF,SPC_LTrim(str));
 
 
-    printf("STR[%s] LEN[%d]\n",str1, strlen(str1));
+    printf("STR[%s] LEN[%ld]\n",str1, strlen(str1));
     SPC_RTrim(str1);
-    printf("STR[%s] LEN[%d]\n",str1, strlen(str1));
+    printf("STR[%s] LEN[%ld]\n",str1, strlen(str1));
 
-    printf("STR[%s] LEN[%d]\n",str2, strlen(str2));
+    printf("STR[%s] LEN[%ld]\n",str2, strlen(str2));
     SPC_RTrim(str2);
-    printf("STR[%s] LEN[%d]\n",str2, strlen(str2));
+    printf("STR[%s] LEN[%ld]\n",str2, strlen(str2));
 
-    printf("STR[%s] LEN[%d]\n",str3, strlen(str3));
+    printf("STR[%s] LEN[%ld]\n",str3, strlen(str3));
     SPC_RTrim(str3);
-    printf("STR[%s] LEN[%d]\n",str3, strlen(str3));
+    printf("STR[%s] LEN[%ld]\n",str3, strlen(str3));
 
-    printf("STR[%s] LEN[%d]\n",str4, strlen(str4));
+    printf("STR[%s] LEN[%ld]\n",str4, strlen(str4));
     SPC_RTrim(str4);
-    printf("STR[%s] LEN[%d]\n",str4, strlen(str4));
+    printf("STR[%s] LEN[%ld]\n",str4, strlen(str4));
 
-    printf("STR[%s] LEN[%d]\n",str5, strlen(str5));
+    printf("STR[%s] LEN[%ld]\n",str5, strlen(str5));
     SPC_RTrim(str5);
-    printf("STR[%s] LEN[%d]\n",str5, strlen(str5));
+    printf("STR[%s] LEN[%ld]\n",str5, strlen(str5));
 
-    printf("STR[%s] LEN[%d]\n",str6, strlen(str6));
+    printf("STR[%s] LEN[%ld]\n",str6, strlen(str6));
     SPC_RTrim(str6);
-    printf("STR[%s] LEN[%d]\n",str6, strlen(str6));
+    printf("STR[%s] LEN[%ld]\n",str6, strlen(str6));
 
-    printf("STR[%s] LEN[%d]\n",str7, strlen(str7));
+    printf("STR[%s] LEN[%ld]\n",str7, strlen(str7));
     SPC_RTrim(str7);
-    printf("STR[%s] LEN[%d]\n",str7, strlen(str7));
+    printf("STR[%s] LEN[%ld]\n",str7, strlen(str7));
 
-    printf("STR[%s] LEN[%d]\n",str8, strlen(str8));
+    printf("STR[%s] LEN[%ld]\n",str8, strlen(str8));
     SPC_RTrim(str8);
-    printf("STR[%s] LEN[%d]\n",str8, strlen(str8));
+    printf("STR[%s] LEN[%ld]\n",str8, strlen(str8));
 
 
 
